fa5df1af3a880b4e42f044cfe71afa6b91d8ab29
StoDruzey <sergelabada@gmail.com>
2023-07-04 23:46:06 +0200

    Bottom navigation

---
+++ app/src/main/java/com/example/sample14l10bottomnavigtion/PageFragment.kt
@@ -0,0 +1,39 @@
+package com.example.sample14l10bottomnavigtion
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sample14l10bottomnavigtion.databinding.FragmentPageBinding
+
+class PageFragment : Fragment() {
+
+    private var _binding: FragmentPageBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentPageBinding.inflate(inflater, container, false)
+            .also { _binding = it }
+            .root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+}
