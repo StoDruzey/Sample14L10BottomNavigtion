ef09bb0389c2606ef029a6b9e249a83c79503724
StoDruzey <sergelabada@gmail.com>
2023-07-05 23:56:20 +0200

    Bottom navigation

--- app/src/main/java/com/example/sample14l10bottomnavigtion/PageFragment.kt
+++ app/src/main/java/com/example/sample14l10bottomnavigtion/PageFragment.kt
@@ -11,9 +11,11 @@ class PageFragment : Fragment() {
 
     private var _binding: FragmentPageBinding? = null
     private val binding get() = requireNotNull(_binding)
+    private var pageText = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        pageText = savedInstanceState?.getString(PAGE_KEY)
     }
 
     override fun onCreateView(
